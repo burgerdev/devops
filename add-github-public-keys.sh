d8e99bcbc93253739e01813360c4f1d76b663e08
Markus Rudy <webmaster@burgerdev.de>
2017-05-06 12:16:41 +0200

    Create add-github-public-keys.sh

---
+++ add-github-public-keys.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+github_user="${GITHUB_USER:-burgerdev}"
+home="${HOME:-/root}"
+
+curl -s "https://api.github.com/users/${github_user}/keys"|grep key|awk  '{print $2 " " $3}'|sed 's/"//g' >> "${home}/.ssh/authorized_keys"
