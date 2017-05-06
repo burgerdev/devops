3914fe1249dabcf8b6705d98f012ffa0a9920344
Markus Rudy <webmaster@burgerdev.de>
2017-05-06 12:03:47 +0200

    Create ubuntu-docker-setup.sh

---
+++ ubuntu-docker-setup.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+export DEBIAN_FRONTEND=noninteractive
+
+apt-get update
+apt-get upgrade -yq
+
+apt-get install -yq \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+apt-get update
+apt-get install -yq docker-ce
+
+systemctl restart docker.service
