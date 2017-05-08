f4cc4c141e3399c368fca2550312334a9df60439
Markus Rudy <webmaster@burgerdev.de>
2017-05-08 21:01:00 +0200

    add kubeadm commands to kubernetes setup

--- ubuntu-kubernetes-setup.sh
+++ ubuntu-kubernetes-setup.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
+## !!!! this file retrieves the pod network insecurely !!!!
+
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" >/etc/apt/sources.list.d/kubernetes.list
 apt-get update
 apt-get install -y kubelet kubeadm kubectl kubernetes-cni
+
+if [[ "$KUBEROLE" == "master" ]];
+then
+    kubeadm init
+    kubectl taint nodes --all node-role.kubernetes.io/master-
+    kubectl apply -f http://docs.projectcalico.org/v2.1/getting-started/kubernetes/installation/hosted/kubeadm/1.6/calico.yaml
+    echo "export KUBECONFIG=/etc/kubernetes/admin.conf" >> $HOME/.bashrc
+fi
