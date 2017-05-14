657a9d81c598335ecf2fd95de2a95a5a8deaec15
Markus Rudy <webmaster@burgerdev.de>
2017-05-14 13:01:27 +0200

    add optional elasticsearch setting to fluentd

--- ubuntu-fluentd-setup.sh
+++ ubuntu-fluentd-setup.sh
@@ -9,3 +9,16 @@ EOF
 
 curl -L https://toolbelt.treasuredata.com/sh/install-ubuntu-xenial-td-agent2.sh | sh
 
+es=`which elasticsearch`
+
+if [[ "$es" != "" ]];
+then
+cat >>/etc/td-agent/td-agent.conf <<EOF
+
+<match *.**>
+  @type elasticsearch
+  logstash_format true
+  flush_interval 10s # for testing
+</match>
+EOF
+fi
