13ed3302723d7c4fc0c8cfcf1a2120483f622c43
Markus Rudy <webmaster@burgerdev.de>
2017-05-18 21:15:08 +0200

    add 'make

--- ubuntu-basic-packages.sh
+++ ubuntu-basic-packages.sh
@@ -5,4 +5,4 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get update
 apt-get upgrade -y
 
-apt-get install -yq vim most git htop pv jq curl wget net-tools
+apt-get install -yq vim most git htop pv jq curl wget net-tools make
