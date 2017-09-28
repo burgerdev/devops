d9ed7e601cae8b03da867a012ad48dd29a147c67
Markus Rudy <webmaster@burgerdev.de>
2017-09-28 22:20:23 +0200

    create ssh dir if necessary

--- add-github-public-keys.sh
+++ add-github-public-keys.sh
@@ -3,4 +3,7 @@
 github_user="${GITHUB_USER:-burgerdev}"
 home="${HOME:-/root}"
 
+mkdir -p "$home/.ssh"
+chmod g=,o= "$home/.ssh"
+
 curl -s "https://api.github.com/users/${github_user}/keys"|grep key|awk  '{print $2 " " $3}'|sed 's/"//g' >> "${home}/.ssh/authorized_keys"
