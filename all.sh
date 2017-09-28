2a379baff67324156a2f8ea37eec8e456823fb97
Markus Rudy <webmaster@burgerdev.de>
2017-09-28 22:21:16 +0200

    silence curl output

--- all.sh
+++ all.sh
@@ -4,5 +4,5 @@ set -e
 
 for f in ubuntu-basic-packages add-dotfiles add-github-public-keys ubuntu-docker-setup ubuntu-kubernetes-setup
 do
-    curl https://raw.githubusercontent.com/burgerdev/devops/master/$f.sh | bash
+    curl -s https://raw.githubusercontent.com/burgerdev/devops/master/$f.sh | bash
 done
